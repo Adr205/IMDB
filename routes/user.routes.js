658c293b04f224f73caaa390ddff098bc6a20b7b
Adrian <A01242469@itesm.mx>
2022-06-18 00:42:52 -0500

    Docker env

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,67 +1,89 @@
-const express = require('express')
-const router = express.Router()
-const User = require('../models/User')
-const mongoose = require('mongoose')
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const mongoose = require("mongoose");
 
-router.get('/', (req, res) => {
+// Single Responsibility Principle
+class Users {
+  constructor(userName, password, email, key) {
+    this.userName = userName;
+    this.password = password;
+    this.email = email;
+    this.key = key;
+  }
+
+  updateKey(key) {
+    this.key = key;
+  }
+}
+
+router.get("/", (req, res) => {
   User.find({}, (err, users) => {
     if (err) {
-      res.status(500).send(err)
+      res.status(500).send(err);
     } else {
-      res.status(200).json(users)
+      res.status(200).json(users);
     }
-  })
-})
+  });
+});
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   User.findById(req.params.id, (err, user) => {
     if (err) {
-      res.status(500).send(err)
+      res.status(500).send(err);
     } else {
-      res.status(200).json(user)
+      res.status(200).json(user);
     }
-  })
-})
+  });
+});
 
-router.post('/', (req, res) => {
-  const user = new User(req.body)
+router.post("/", (req, res) => {
+  const user = new User(req.body);
 
-  user.password = user.encryptPassword(user.password)
+  user.password = user.encryptPassword(user.password);
 
   user.save((err, user) => {
     if (err) {
-      res.status(500).send(err)
+      res.status(500).send(err);
     } else {
-      res.status(201).json(user)
+      res.status(201).json(user);
     }
-  })
-})
+  });
+});
 
-router.put('/:id', (req, res) => {
-  const { key } = req.body
+router.put("/:id", async (req, res) => {
+  const { key } = req.body;
+  const { id } = req.params;
 
-  User.findByIdAndUpdate(
-    req.params.id,
-    { $set: { key } },
-    { new: true },
-    (err, user) => {
-      if (err) {
-        res.status(500).send(err)
-      } else {
-        res.status(200).json(user)
-      }
+  const userHelper = await User.findById(id);
+
+  if (userHelper) {
+    const user = new Users(
+      userHelper.userName,
+      userHelper.password,
+      userHelper.email,
+      key
+    );
+    user.updateKey(key);
+    const updateKey = await User.findByIdAndUpdate(id, user, { new: true });
+    if (updateKey) {
+      res.status(200).json(updateKey);
+    } else {
+      res.status(500).send("Error");
     }
-  )
-})
+  } else {
+    res.status(404).json({ ok: false, message: "User not found" });
+  }
+});
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   User.findByIdAndRemove(req.params.id, (err, user) => {
     if (err) {
-      res.status(500).send(err)
+      res.status(500).send(err);
     } else {
-      res.status(200).json({ ok: true, message: 'User deleted' })
+      res.status(200).json({ ok: true, message: "User deleted" });
     }
-  })
-})
+  });
+});
 
-module.exports = router
+module.exports = router;
